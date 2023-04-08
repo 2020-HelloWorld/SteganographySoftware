b3d9505ec5ffef07f5107f008b33f770389a42aa
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2023-04-09 05:07:02 +0530

    non functional routes

--- backend/Flask/website/views.py
+++ backend/Flask/website/views.py
@@ -1,12 +1,78 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 
 views = Blueprint('views', __name__)
 
+#FUNTIONAL ROUTES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# NON-FUNTIONAL ROUTES
 
 @views.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST': 
-        pass
-        # ip = request.form.get('ip')#Gets the note from the HTML 
-            # flash('Error joining Mesh Network', category='error') 
-    # return render_template("connect.html")
+def landing():
+    return render_template("landing.html")
+
+@views.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template("dashboard.html")
+
+#--- DASHBOARD ROUTES
+
+@views.route('/covert', methods=['GET', 'POST'])
+def covertdash():
+    return render_template("covertlist.html")
+
+@views.route('/local', methods=['GET', 'POST'])
+def localdash():
+    return render_template("locallist.html")
+
+#--- COVERT ROUTES
+
+@views.route('/burst', methods=['GET', 'POST'])
+def burst():
+    return render_template("covert-burst.html")
+
+@views.route('/timestamp', methods=['GET', 'POST'])
+def timestamp():
+    return render_template("covert-timestamp.html")
+
+#--- LOCAL ROUTES
+
+@views.route('/anytoany', methods=['GET', 'POST'])
+def anytoany():
+    return render_template("any-file.html")
+
+@views.route('/texttoimg', methods=['GET', 'POST'])
+def texttoimg():
+    return render_template("text-to-image.html")
+
+@views.route('/texttoaudio', methods=['GET', 'POST'])
+def texttoaudio():
+    return render_template("text-to-audio.html")
